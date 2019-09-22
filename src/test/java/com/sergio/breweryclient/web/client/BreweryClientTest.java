596ff1904e71e5569a7616cb227927ced922da80
sergiopoliveira <sergiopoliveira@outlook.com>
2019-09-22 14:58:05 +0100

    delete

--- src/test/java/com/sergio/breweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/sergio/breweryclient/web/client/BreweryClientTest.java
@@ -36,4 +36,9 @@ class BreweryClientTest {
         BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
         client.updateBeer(UUID.randomUUID(), beerDto);
     }
+
+    @Test
+    void testDeleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
+    }
 }
