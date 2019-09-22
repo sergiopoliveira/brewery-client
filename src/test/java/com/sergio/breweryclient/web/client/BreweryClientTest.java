8e39e0eb7a56a91b2187ebcbc079e531c5b55d37
sergiopoliveira <sergiopoliveira@outlook.com>
2019-09-22 14:51:23 +0100

    post

--- src/test/java/com/sergio/breweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/sergio/breweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +20,13 @@ class BreweryClientTest {
     void getBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
     }
 }
