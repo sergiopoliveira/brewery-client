4cb0c49b63101ef318bdf5346ca8b2d539eb7fc6
sergiopoliveira <sergiopoliveira@outlook.com>
2019-10-22 20:27:39 +0100

    constraints

--- src/main/java/com/sergio/breweryclient/web/model/BeerDto.java
+++ src/main/java/com/sergio/breweryclient/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 
@@ -14,8 +18,15 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @NotNull
     private String beerName;
+
+    @NotBlank
     private String beerStyle;
+
+    @Positive
     private Long upc;
 }
