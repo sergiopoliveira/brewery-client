0736b6617d53995ceecc48adfafe8fe01bca9c97
sergiopoliveira <sergiopoliveira@outlook.com>
2019-10-27 16:52:22 +0000

    Customer Serializer with Jackson

--- src/test/java/com/sergio/breweryclient/model/BaseTest.java
+++ src/test/java/com/sergio/breweryclient/model/BaseTest.java
@@ -5,6 +5,7 @@ import com.sergio.breweryclient.web.model.BeerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -21,6 +22,7 @@ public class BaseTest {
                 .upc(123123123123L)
                 .price(BigDecimal.valueOf(12.99d))
                 .creationDate(OffsetDateTime.now())
+                .myLocalDate(LocalDate.now())
                 .build();
     }
 
