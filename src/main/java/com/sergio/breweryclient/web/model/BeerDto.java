9adf28e58757222d440a3952a9f80336b90909ad
sergiopoliveira <sergiopoliveira@outlook.com>
2019-10-27 17:08:58 +0000

    Custom Deserializer with Jackson

--- src/main/java/com/sergio/breweryclient/web/model/BeerDto.java
+++ src/main/java/com/sergio/breweryclient/web/model/BeerDto.java
@@ -2,6 +2,7 @@ package com.sergio.breweryclient.web.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -44,5 +45,6 @@ public class BeerDto {
     private OffsetDateTime creationDate;
 
     @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate myLocalDate;
 }
